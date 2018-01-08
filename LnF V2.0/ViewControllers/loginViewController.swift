6ab01a64891055d6dbf8839d18d9c85350e52418
Ayush Verma <ayush.verma2016@vitstudent.ac.in>
2018-01-09 01:18:14 +0530

    Files made for every class and module in the application.

---
+++ LnF V2.0/ViewControllers/loginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  loginViewController.swift
+//  LnF V2.0
+//
+//  Created by Ayush Verma on 09/01/18.
+//  Copyright © 2018 Ayush Verma. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.resignFirstResponder()
+        self.view.endEditing(true)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+//end of the class brace
+}
